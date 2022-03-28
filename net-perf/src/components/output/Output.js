dd912d440ddc677e6f2db1c2530daf7ed8dca9fa
kimtaeseoung <aka404365@gmail.com>
2022-03-28 13:37:45 +0900

    [feat] UI 1차  구성완료

--- net-perf/src/components/output/Output.js
+++ net-perf/src/components/output/Output.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
-
+import MyResponsiveLine from './MyResponsiveLine';
+import FtraceList from './FtraceList';
 const Output = props => {
-	return <></>;
+	return (
+		<>
+			<div id="Output_container">
+				<FtraceList></FtraceList>
+				<div id="graph_container">
+					<MyResponsiveLine></MyResponsiveLine>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Output;
