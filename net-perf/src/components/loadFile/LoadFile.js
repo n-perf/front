5d8c7d73f540cb64142e371d43c22965256f3088
kimtaeseoung <aka404365@gmail.com>
2022-03-28 19:29:59 +0900

    [feat] 파일명 출력 기능 추가

--- net-perf/src/components/loadFile/LoadFile.js
+++ net-perf/src/components/loadFile/LoadFile.js
@@ -1,8 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const LoadFile = props => {
+	const inputRef = useRef(null);
+
 	const onExecuteClick = e => {
 		props.setOnRunning(true);
+	};
+
+	const [filenames, setFilenames] = useState(['', '', '']);
+
+	const chooseFile = e => {
+		console.log(
+			'hhhh제발 좀 되라!!제발 좀 되라!!제발 좀 되라!!제발 좀 되라!!제발 좀 되라!!h1111111',
+		);
+		let file = e.target.files[0];
+		// var file = inputRef.current.file[0];
+		setFilenames([file.name, '', '']);
+		console.log(
+			'hhhh제발 좀 되라!!제발 좀 되라!!제발 좀 되라!!제발 좀 되라!!제발 좀 되라!!h222222',
+		);
 	};
 
 	return (
@@ -11,22 +27,42 @@ const LoadFile = props => {
 			<div id="LoadFile_container">
 				<div id="upload_container">
 					<div className="upload_div">
-						<label className="uploadButton" for="input-file">
+						<label className="uploadButton" for="file_1">
 							Select File
 						</label>
-						<input type="file" id="input-file" style={{ display: 'none' }} />
+						<input
+							type="file"
+							className="input-file"
+							id="file_1"
+							style={{ display: 'none' }}
+							onChange={chooseFile}
+							ref={inputRef}
+						/>
+						<p id="file_json">{filenames[0]}</p>
 					</div>
 					<div className="upload_div">
-						<label className="uploadButton" for="input-file">
+						<label className="uploadButton" for="file_2">
 							Select File
 						</label>
-						<input type="file" id="input-file" style={{ display: 'none' }} />{' '}
+						<input
+							type="file"
+							className="input-file"
+							id="file_2"
+							style={{ display: 'none' }}
+							onChange={chooseFile}
+						/>
 					</div>
 					<div className="upload_div">
-						<label className="uploadButton" for="input-file">
+						<label className="uploadButton" for="file_3">
 							Select File
 						</label>
-						<input type="file" id="input-file" style={{ display: 'none' }} />{' '}
+						<input
+							type="file"
+							className="input-file"
+							id="file_3"
+							style={{ display: 'none' }}
+							onChange={chooseFile}
+						/>
 					</div>
 				</div>
 				<div id="execute" onClick={onExecuteClick}>
