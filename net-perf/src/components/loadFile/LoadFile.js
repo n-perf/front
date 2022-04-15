e79e131e707dca795b99b10d6f94d7e3c4f4cd44
kimtaeseoung <aka404365@gmail.com>
2022-04-15 19:34:53 +0900

    [feat] 업로드 버튼 컴포넌트화, 파일 업로드시 버튼 밑에 파일이름 출력 기능 추가

--- net-perf/src/components/loadFile/LoadFile.js
+++ net-perf/src/components/loadFile/LoadFile.js
@@ -2,17 +2,46 @@ import React, { useState, useEffect, useRef } from 'react';
 import Uploadbtn from './Uploadbtn';
 
 const LoadFile = props => {
+	const [filenames, setFilenames] = useState({ 0: '', 1: '', 2: '' });
+
+	useEffect(() => {
+		console.log('나타났을때만 호출', filenames);
+	}, [filenames]);
+
 	const onExecuteClick = e => {
 		props.setOnRunning(true);
+	};
+
+	const chooseFile = e => {
+		switch (e.target.id) {
+			case '0':
+				setFilenames(prevState => ({
+					...prevState,
+					0: e.target.files[0].name,
+				}));
+				break;
+			case '1':
+				setFilenames(prevState => ({
+					...prevState,
+					1: e.target.files[0].name,
+				}));
+				break;
+			case '2':
+				setFilenames(prevState => ({
+					...prevState,
+					2: e.target.files[0].name,
+				}));
+				break;
+		}
 	};
 
 	return (
 		<>
 			<div id="LoadFile_container">
 				<div id="upload_container">
-					<Uploadbtn></Uploadbtn>
-					<Uploadbtn></Uploadbtn>
-					<Uploadbtn></Uploadbtn>
+					<Uploadbtn seq={'0'} filenames={filenames[0]} chooseFile={chooseFile}></Uploadbtn>
+					<Uploadbtn seq={'1'} filenames={filenames[1]} chooseFile={chooseFile}></Uploadbtn>
+					<Uploadbtn seq={'2'} filenames={filenames[2]} chooseFile={chooseFile}></Uploadbtn>
 				</div>
 				<div id="execute" onClick={onExecuteClick}>
 					<div>Performance Test</div>
