1dcc94b7eb8b0f2347dbaa6fee0d9646beee8758
kimtaeseoung <aka404365@gmail.com>
2022-04-15 19:34:53 +0900

    [feat] 업로드 버튼 컴포넌트화, 파일 업로드시 버튼 밑에 파일이름 출력 기능 추가

--- net-perf/src/components/loadFile/Uploadbtn.js
+++ net-perf/src/components/loadFile/Uploadbtn.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 const Uploadbtn = props => {
-	const [filenames, setFilenames] = useState('');
+	// const [filenames, setFilenames] = useState('');
 	// props
 
-	const chooseFile = e => {
-		let file = e.target.files[0];
-		// var file = inputRef.current.file[0];
-		setFilenames(file.name);
-	};
+	// const chooseFile = e => {
+	// 	let file = e.target.files[0];
+	// 	// var file = inputRef.current.file[0];
+	// 	setFilenames(file.name);
+	// };
 
 	return (
 		<>
 			<div className="upload_div">
-				<label className="uploadButton" for="filebtn">
+				<label className="uploadButton" for={props.seq}>
 					Select File
 				</label>
 				<input
 					type="file"
 					className="input-file"
-					id="filebtn"
+					id={props.seq}
 					style={{ display: 'none' }}
-					onChange={chooseFile}
+					onChange={props.chooseFile}
 				/>
-				<p id="file_json">{filenames}</p>
+				<p id="file_json">{props.filenames}</p>
 			</div>
 		</>
 	);
