4e7f6411dd9893ebbf10c44c1600b597ade1f8c9
Juhee Kang <claudiajkang@gmail.com>
2022-05-18 13:30:45 +0900

    [feat] add computeLcs for calculating lcs array

--- net-perf/src/differ.js
+++ net-perf/src/differ.js
@@ -60,5 +60,30 @@ function prepareDiff(text) {
     return results;
 }
 
+function computeLcs(before, after) {
+    let beforeLen = before.length;
+    let afterLen = after.length;
+    let i, j;
+
+    let lcs = new Array(beforeLen + 1).fill(0);
+    for (i = 0; i < beforeLen + 1; i++) {
+        lcs[i] = new Array(afterLen + 1).fill(0);
+    }
+
+    for (i = 0; i < beforeLen + 1; i++) {
+        for (j = 0; j < afterLen + 1; j++) {
+            if (i === 0 || j === 0) {
+                lcs[i][j] = 0;
+            } else if (before[i - 1] === after[j - 1]) {
+                lcs[i][j] = 1 + lcs[i - 1][j - 1];
+            } else {
+                lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+            }
+        }
+    }
+
+    return lcs;
+}
+
 const before = beforeFtrace.split('\n');
 const after = afterFtrace.split('\n');
