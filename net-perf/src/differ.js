5f7bf6ead89b8377a68ac6ad9b5208f913263046
Juhee Kang <claudiajkang@gmail.com>
2022-05-18 13:28:31 +0900

    [chore] add dummy data for diff

---
+++ net-perf/src/differ.js
@@ -0,0 +1,51 @@
+const beforeFtrace = "# tracer: function_graph\n" +
+    "#\n" +
+    "# CPU  DURATION                  FUNCTION CALLS\n" +
+    "# |     |   |                     |   |   |   |\n" +
+    "  6)               |  hsr_netdev_notify [hsr]() {\n" +
+    "  6)               |    rtnl_is_locked() {\n" +
+    "  6)   0.160 us    |      mutex_is_locked();\n" +
+    "  6)   0.770 us    |    }\n" +
+    "  6)   0.150 us    |    hsr_port_exists [hsr]();\n" +
+    "  6)   0.150 us    |    is_hsr_master [hsr]();\n" +
+    "  6)   3.500 us    |  }\n" +
+    "  6)               |  hsr_netdev_notify [hsr]() {\n" +
+    "  6)               |    rtnl_is_locked() {\n" +
+    "  6)   0.090 us    |      mutex_is_locked();\n" +
+    "  6)   0.240 us    |    }\n" +
+    "  6)   0.080 us    |    hsr_port_exists [hsr]();\n" +
+    "  6)   0.090 us    |    is_hsr_master [hsr]();\n" +
+    "  6)   0.810 us    |  }\n" +
+    " 13)               |  hsr_netdev_notify [hsr]() {\n" +
+    " 13)               |    rtnl_is_locked() {\n" +
+    " 13)   0.160 us    |      mutex_is_locked();\n" +
+    " 13)   0.660 us    |    }\n" +
+    " 13)   0.140 us    |    hsr_port_exists [hsr]();\n" +
+    " 13)   0.130 us    |    is_hsr_master [hsr]();\n" +
+    " 13)   3.110 us    |  }";
+
+const afterFtrace = "# tracer: function_graph\n" +
+    "#\n" +
+    "# CPU  DURATION                  FUNCTION CALLS\n" +
+    "# |     |   |                     |   |   |   |\n" +
+    "  6)               |  hsr_netdev_notify [hsr]() {\n" +
+    "  6)               |    rtnl_is_locked() {\n" +
+    "  6)   0.260 us    |      mutex_is_locked();\n" +
+    "  6)   0.370 us    |    }\n" +
+    "  6)   0.450 us    |    hsr_port_exists [hsr]();\n" +
+    "  6)   0.550 us    |    is_hsr_master [hsr]();\n" +
+    "  6)   3.500 us    |  }\n" +
+    "  6)               |  hsr_netdev_notify [hsr]() {\n" +
+    "  6)               |    rtnl_is_locked() {\n" +
+    "  6)   0.090 us    |      mutex_is_locked();\n" +
+    "  6)   0.240 us    |    }\n" +
+    "  6)   0.080 us    |    hsr_port_exists [hsr]();\n" +
+    "  6)   0.090 us    |    is_hsr_master [hsr]();\n" +
+    "  6)   0.110 us    |  }\n" +
+    " 13)               |  hsr_netdev_notify [hsr]() {\n" +
+    " 13)               |    rtnl_is_locked() {\n" +
+    " 13)   0.260 us    |      mutex_is_locked();\n" +
+    " 13)   0.660 us    |    }\n" +
+    " 13)   0.140 us    |    hsr_port_exists [hsr]();\n" +
+    " 13)   0.130 us    |    is_hsr_master [hsr]();\n" +
+    " 13)   3.110 us    |  }";
