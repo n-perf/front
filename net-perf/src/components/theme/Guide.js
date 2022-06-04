32dc192d4f524f309c49420ef323a45f9e38f0ad
t3qquq <t3qquq@gmail.com>
2022-06-04 14:10:41 +0900

    fetched origin from upstream

--- net-perf/src/components/theme/Guide.js
+++ net-perf/src/components/theme/Guide.js
@@ -1,13 +1,13 @@
-import React,{useContext} from 'react';
-import "shepherd.js/dist/css/shepherd.css";
-import { ShepherdTour, ShepherdTourContext } from "react-shepherd";
+import React, { useContext } from 'react';
+import 'shepherd.js/dist/css/shepherd.css';
+import { ShepherdTour, ShepherdTourContext } from 'react-shepherd';
 const Guide = props => {
-    const tour = useContext(ShepherdTourContext);
+	const tour = useContext(ShepherdTourContext);
 	return (
-         <button class="btn btn-primary btn-jittery"  onClick={tour.start}>
-        Start Tour
-        </button>
-      );
+		<button class="btn btn-primary btn-jittery" onClick={tour.start}>
+			Start Tour
+		</button>
+	);
 };
 
-export default  Guide;
+export default Guide;
